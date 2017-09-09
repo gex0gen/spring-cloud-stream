c0967cccfc1100b1fa85575d3890e8d80bf25258
Teymur Abdullaev <TEYMURA@amdocs.com>
2017-09-09 20:06:56 +0300

    test

--- spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/BindableAdapter.java
+++ spring-cloud-stream/src/main/java/org/springframework/cloud/stream/binding/BindableAdapter.java
@@ -22,6 +22,7 @@ import java.util.Set;
 /**
  * Default adapter implementation for {@Bindable}.
  *
+ * what what what
  * @author Ilayaperumal Gopinathan
  */
 public class BindableAdapter implements Bindable {
